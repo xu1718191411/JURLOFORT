0b014b00ece2c4b9f889238b0d8191616abbb943
xu1718191411 <xuzhongwei@pc07-22.is.kochi-u.ac.jp>
2016-06-22 17:01:19 +0900

    a

--- bin/install.js
+++ bin/install.js
@@ -8,7 +8,21 @@ var Steps = require('ocsteps');
 
 function initialDatabase(that){
     var database = ["LatestAnalysisMsg","LatestStatementMsg","analysisLog","debateMembers","debateStatus","statementLog","themes","userStatus"]
+    var members = [
+        {
+            "username": "miyoshi",
+            "password": "miyoshi",
+            "type": 0,
+            "group": "miyoshi"
+        },
+        {
+            "username": "syoui",
+            "password": "syoui",
+            "type": 1,
+            "group": "miyoshi"
+        }
 
+    ]
 
 
     var fn = function(_i,_this){
@@ -19,9 +33,22 @@ function initialDatabase(that){
 
             mongo.createIfNotExists(database[_i],_this.hold(function(_res){
                     console.log(_res)
+                    console.log(database[_i])
+                    if(_res == 0 && database[_i] == "debateMembers"){
+                            for(var s=0;s<members.length;s++){
+                                (function(_s,__this){
+                                    __this.step(function(){
+                                        mongo.insert(database[_i],members[_s],{},function(){
+
+                                        })
+                                    })
+                                })(s,_this)
+                            }
+                    }
             }))
         })
     }
+
 
     for(var i=0;i<database.length;i++){
         console.log(1111)
